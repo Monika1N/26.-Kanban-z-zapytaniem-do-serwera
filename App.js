785c5edb7cefadd536471e4fdba79193a2211699
Monika N <monia.niemczyk1@gmail.com>
2018-05-08 11:43:10 +0200

    Modified App.js

--- App.js
+++ App.js
@@ -1,13 +1,3 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
-}
-
 // TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
 var todoColumn = new Column('To do');
 var doingColumn = new Column('Doing');
@@ -25,3 +15,38 @@ var card2 = new Card('Create new kanban board');
 // DODAWANIE KART DO KOLUMN
 todoColumn.createCard(card1);
 doingColumn.createCard(card2);
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3180',
+  'X-Auth-Token': 'a21cdd7f54a2cbd7663b8d07c251d233'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	})
+}
+
+
